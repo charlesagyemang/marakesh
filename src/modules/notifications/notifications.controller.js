2a5e57ba298639a3a4721833f86f97d0f0c6e0f0
charles agyemang <micnkru@gmail.com>
2019-07-23 13:02:05 +0000

    Feat: change the uer email

--- src/modules/notifications/notifications.controller.js
+++ src/modules/notifications/notifications.controller.js
@@ -4,7 +4,7 @@ export const sendEmail = async (recepient, subject, message) => {
   try {
     const client = new Client('fe1f6bb9-aee7-4a25-bd58-1067441563db');
     client.sendEmail({
-      From: 'charles@vendyads.com',
+      From: 'response@vendyads.com',
       To: recepient,
       Subject: subject,
       TextBody: message,
