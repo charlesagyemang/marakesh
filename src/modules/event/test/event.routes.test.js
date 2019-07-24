198ccca12c00e8c25d56a8a35122623828069192
charles agyemang <micnkru@gmail.com>
2019-07-24 21:39:41 +0000

    Feat :

--- src/modules/event/test/event.routes.test.js
+++ src/modules/event/test/event.routes.test.js
@@ -9,7 +9,7 @@ describe('Event:Routes', async () => {
     await nuke();
   });
 
-  it.only('Create Event Successfuly', async () => {
+  it('Create Event Successfuly', async () => {
     await request(server).post('/api/users/register').send({
       name: 'Charles',
       email: 'mm@gmail.com',
@@ -29,6 +29,33 @@ describe('Event:Routes', async () => {
 
     const res = await request(server).get(`/api/events/${event.body.id}`).set(auth);
     console.log(res.body);
+    // expect(res.statusCode).toBe(HTTPStatus.OK);
+    // expect(res.body.id).toBe(event.id);
+  });
+
+  it.only('Edit Event Successfuly', async () => {
+    await request(server).post('/api/users/register').send({
+      name: 'Charles',
+      email: 'mm@gmail.com',
+      password: 'password',
+    });
+
+    const { body } = await request(server).post('/api/users/login').send({
+      email: 'mm@gmail.com',
+      password: 'password',
+    });
+
+    const auth = { Authorization: `Bearer ${body.token}` };
+    const event = await request(server).post('/api/events/').set(auth).send({
+      name: 'qweqweq',
+      createdBy: body.id,
+    });
+
+    const res = await request(server).patch(`/api/events/${event.body.id}`).set(auth).send({
+      name: 'quiiliwa',
+    });
+
+    console.log(res.statusCode);
     // expect(res.statusCode).toBe(HTTPStatus.OK);
     // expect(res.body.id).toBe(event.id);
   });
