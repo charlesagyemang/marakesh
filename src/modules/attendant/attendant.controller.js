ac99e963616846eda6ad22bd631cb1df1f2d660b
charles agyemang <micnkru@gmail.com>
2019-07-25 16:12:05 +0000

    Put in try

--- src/modules/attendant/attendant.controller.js
+++ src/modules/attendant/attendant.controller.js
@@ -28,6 +28,11 @@ export const createAttendant = async (req, res) => {
     event.name,
     event.pluLink,
     `https://xenodochial-dijkstra-e42e41.netlify.com/ratings.html?sID=${req.body.specialId}&eID=${event.id}&rMsg=${event.ratingMessage}&logoUrl=${event.logoUrl}`);
+  } catch (e) {
+    console.log(e);
+  }
+
+  try {
     await sendTextMessage(req.body.phoneNumber, event.name, `Welcome To ${event.name}. Click ${event.pluLink} to access the program. By Brand Sauce`);
   } catch (e) {
     console.log(e);
